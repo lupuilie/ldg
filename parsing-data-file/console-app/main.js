df1ec083d6a4dc9352b50e52bc6b9afc62be891f
Lupu Ilie <lupuiliealexandru@gmail.com>
2022-04-11 13:48:49 +0300

    Update

--- parsing-data-file/console-app/main.js
+++ parsing-data-file/console-app/main.js
@@ -1,20 +1,41 @@
 import * as fs from "fs/promises";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 import parseCSV from "file-parser-il";
-
-async function readFile() {
-  const fileLocation = "./testfile.csv";
-  return await fs.readFile(fileLocation, { encoding: "utf8" });
-}
 
 function display(parsedCSV) {
   console.log(parsedCSV);
 }
 
-async function main() {
-  const data = await readFile();
-  const parsedCSV = parseCSV(data, { headers: ["First", "Last", "Salary"] });
+function formatFileName(name) {
+  return `./${name}.csv`;
+}
 
-  display(parsedCSV);
+async function readFile() {
+  try {
+    const rl = readline.createInterface({ input, output });
+    const fileName = await rl.question(
+      "Input the name of CSV file you want to parse:"
+    );
+    const location = formatFileName(fileName);
+    rl.close();
+
+    const file = await fs.readFile(location, { encoding: "utf8" });
+    return file;
+  } catch (err) {
+    throw new Error("Could not load file. Try again.");
+  }
+}
+
+async function main() {
+  try {
+    const data = await readFile();
+    const parsedCSV = parseCSV(data, { headers: ["First", "Last", "Salary"] });
+
+    display(parsedCSV);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 main();
--- parsing-data-file/file-parser-il/test/csv-parser.test.js
+++ parsing-data-file/file-parser-il/test/csv-parser.test.js
@@ -1,15 +1,14 @@
 import * as fs from "fs/promises";
 import parseCSV from "../csv-parser";
 
-describe("given file testfile.csv, should return the content in a readable format", () => {
-  test("readableFormat", async () => {
-    try {
-      const fileLocation = "./test/testfile.csv";
-      const data = await fs.readFile(fileLocation, { encoding: "utf8" });
-      const parsed = parseCSV(data, { headers: ["First", "Last", "Salary"] });
-      console.log(parsed);
-    } catch (e) {
-      console.error(e);
-    }
+describe("parseCSV", () => {
+  test("given file testfile.csv, parseCSV() should return the content in a readable format", async () => {
+    const fileLocation = "./test/testfile.csv";
+    const data = await fs.readFile(fileLocation, { encoding: "utf8" });
+    const parsed = parseCSV(data, { headers: ["First", "Last", "Salary"] });
+    const expected =
+      "First    Last     Salary\n-------- -------- ------\nLing     Mai      55900 \nJohnson  Jim      56500 \nJones    Aaron    46000 \nJones    Chris    34500 \nSwift    Geoffrey 14200 \nXiong    Fong     65000 \nZarnecki Sabrina  51500 \n";
+
+    expect(parsed).toBe(expected);
   });
 });
