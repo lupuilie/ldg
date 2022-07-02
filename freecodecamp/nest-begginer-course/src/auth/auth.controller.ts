ec6bb517f2f7fc4365946506f6968d881fd7de34
lupuilie <lupuiliealexandru@gmail.com>
2022-07-02 20:13:56 +0300

    added docker container + prisma client

--- freecodecamp/nest-begginer-course/src/auth/auth.controller.ts
+++ freecodecamp/nest-begginer-course/src/auth/auth.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Get()
+  index() {
+    return this.authService.routes();
+  }
 
   @Post('signup')
   signup() {
--- freecodecamp/nest-begginer-course/src/auth/auth.service.ts
+++ freecodecamp/nest-begginer-course/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  routes() {
+    return {
+      login: `http://login`,
+      signup: `http://signup`,
+    };
+  }
+  login() {
+    return 'login';
+  }
+  signup() {
+    return 'signup';
+  }
+}
